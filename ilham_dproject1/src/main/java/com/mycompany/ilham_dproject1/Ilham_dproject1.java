e185d885c807aa4225368a555a9da68e2a33d0ce
Pangestu Ilham Dwi Prastyo <pangestuilhamdwiprastyo@gmail.com>
2026-08-26 16:59:34 +0700

    initial commit

---
+++ ilham_dproject1/src/main/java/com/mycompany/ilham_dproject1/Ilham_dproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ilham_dproject1;
+
+/**
+ *
+ * @author Ilham Dwi
+ */
+public class Ilham_dproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Pangestu Ilham Dwi Prastyo");
+        System.out.println("NPM : 2505060026");
+        System.out.println("Alamat : Kebumen, Pringsurat, Temanggung");
+        System.out.println("No_HP : 085727608660");
+    }
+}
